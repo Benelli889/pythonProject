e61a6960fe6df551302ec757a9799167d33e81c6
Arndt <arndt.steffens@mail.de>
2021-08-25 23:47:23 +0200

    first

--- main.py
+++ main.py
@@ -2,17 +2,24 @@
 
 # Press Umschalt+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import os
+import numpy as np
 
 
-def print_hi(name):
+def print_hi(name,no):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+    return 111+i,222-i
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     for i in range(5):
-        print_hi(f'PyCharm {i}')
+        a,b = print_hi(f'PyCharm {i}',i)
+        print (f'a: {a} b: {b}')
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+l = [5,4,6,7,6,8,7,6,7,8]
+print (l)
+print (np.sin(3))
